7875535c99742f40fd892724fef290a76764aee6
Massimiliano-Novello <massinov5@gmail.com>
2023-04-11 15:51:09 +0200

    inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+
+createApp({
+    data() {
+      return {
+        email: []
+      }
+    },
+    mounted() {
+        for (let i = 0; i > 10; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    console.log(resp);
+                    const generateEmail = resp.data.response;
+                    
+                 });
+        }
+    }
+}).mount("#app")
