b6cc522e6ae7e2202c6a640f248382ef499eb11f
Massimiliano-Novello <massinov5@gmail.com>
2023-04-11 16:31:37 +0200

    fine eserciozio + bonus

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-const { createApp } = Vue
-
+const { createApp } = Vue;
 
 createApp({
-    data() {
-      return {
-        email: []
-      }
-    },
-    mounted() {
-        for (let i = 0; i > 10; i++) {
-            axios
-                .get("https://flynn.boolean.careers/exercises/api/random/mail")
-                .then((resp) => {
-                    console.log(resp);
-                    const generateEmail = resp.data.response;
-                    
-                 });
-        }
+  data() {
+    return {
+      email: [],
+    };
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((resp) => {
+          console.log(resp);
+          const generateEmail = resp.data.response;
+
+          this.email.push(generateEmail);
+        });
     }
-}).mount("#app")
+  },
+}).mount("#app");
